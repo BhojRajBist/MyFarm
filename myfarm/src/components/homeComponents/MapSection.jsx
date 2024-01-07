8330b3978b59193e40a250fdd1e471aac783bbd8
Bhoj Raj Bist <bhojbist1@gmail.com>
2024-01-07 21:49:33 +0545

    Map with Geolocation added

--- myfarm/src/components/homeComponents/MapSection.jsx
+++ myfarm/src/components/homeComponents/MapSection.jsx
@@ -1,13 +1,32 @@
 // src/components/MapSection.jsx
-import React from 'react';
 import './MapSection.css';
 
+import React, { useEffect } from 'react';
+import '@maptiler/sdk/dist/maptiler-sdk.css';
+import * as maptilersdk from '@maptiler/sdk';
+
 const MapSection = () => {
+  useEffect(() => {
+    maptilersdk.config.apiKey = 'i5SNt5gzLB42EuF1VtPa';
+
+    const map = new maptilersdk.Map({
+      container: 'map', // container's id or the HTML element in which SDK will render the map
+      style: maptilersdk.MapStyle.STREETS,
+      geolocate: maptilersdk.GeolocationType.POINT
+    });
+
+    return () => {
+      // Clean up if needed
+      map.remove();
+    };
+  }, []); // Empty dependency array to run the effect only once on mount
+
   return (
-    <section className="map-section">
-      {/* Map content goes here */}
-    </section>
+    <div>
+      {/* You can add any additional content or components here */}
+      <div id="map" style={{ height: '100vh' }}></div>
+    </div>
   );
-}
+};
 
 export default MapSection;
